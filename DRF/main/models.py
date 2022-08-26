41550417abedb7a7c4235112d6c8c2b80ecfdcf7
L0L-kek-31415 <tanyaboiko1209@gmail.com>
2022-08-26 18:42:05 +0300

    api

--- DRF/main/models.py
+++ DRF/main/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django_countries.fields import CountryField
 
-from app.DRF.main.abstract_models import CreatedAt, UpdatedAt, SoftDelete
+from main.abstract_models import CreatedAt, UpdatedAt, SoftDelete
+
+
+# from DRF.main.abstract_models import CreatedAt, UpdatedAt, SoftDelete
 
 
 class Offices(CreatedAt, UpdatedAt, SoftDelete):
---
+++ DRF/main/views.py
@@ -0,0 +1,52 @@
+from rest_framework import generics, viewsets, permissions
+
+from main.models import (Company, Employer,
+                             Offices, Person, Skills,
+                             Languages,  Level)
+from main.permissions import IsAdminOrReadOnly
+from main.serializers import (OfficeSerializer, CompanySerializer,
+                                  PersonSerializer, EmployerSerializer,
+                                  SkillSerializer, LanguagesSerializer,
+                                  LevelSerializer)
+
+
+class OfficeViewSet(viewsets.ModelViewSet):
+    queryset = Offices.objects.filter(is_active=True)
+    serializer_class = OfficeSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.filter(is_active=True)
+    serializer_class = CompanySerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.filter(is_active=True)
+    serializer_class = PersonSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class EmployerViewSet(viewsets.ModelViewSet):
+    queryset = Employer.objects.filter(is_active=True)
+    serializer_class = EmployerSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class SkillViewSet(viewsets.ModelViewSet):
+    queryset = Skills.objects.all()
+    serializer_class = SkillSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+
+
+class LanguagesViewSet(viewsets.ModelViewSet):
+    queryset = Languages.objects.all()
+    serializer_class = LanguagesSerializer
+    permission_classes = (IsAdminOrReadOnly,)
